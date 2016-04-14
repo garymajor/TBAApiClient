9f5794af043fc4a3a5f7478b09e210f03ca5b07b
Gary Major <garymajor@hotmail.com>
2016-04-14 11:04:28 -0700

    Added wrapper class for the team and event APIs

---
+++ TbaApiClient/TbaApiClient/ApiClientClasses/ApiClient.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TbaApiClient
+{
+    /// <summary>
+    /// The Blue Alliance API Client class
+    /// </summary>
+    public class ApiClient
+    {
+        /// <summary>
+        /// Event API Class
+        /// </summary>
+        public Event EventApi
+        {
+            get
+            {
+                return eventApi;
+            }
+        }
+
+        /// <summary>
+        /// Event API Class
+        /// </summary>
+        public Team TeamApi
+        {
+            get
+            {
+                return teamApi;
+            }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ApiClient()
+        {
+            eventApi = new Event();
+            teamApi = new Team();
+        }
+
+        /// <summary>
+        /// private Event API member
+        /// </summary>
+        private Event eventApi { get; set; }
+
+        /// <summary>
+        /// private Team API member
+        /// </summary>
+        private Team teamApi { get; set; }
+    }
+}
